aa2794a30d085176b3717df622a8df1fc5ebd1c4
Pragyan Dash <68701602+pragyan7@users.noreply.github.com>
2024-06-21 20:20:47 +0530

    Update main.py
    used list comprehension at line 21

--- U.S. States Game/main.py
+++ U.S. States Game/main.py
@@ -17,7 +17,11 @@ guessed_states = []
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title = f"{len(guessed_states)}/50 States Correct", 
                                     prompt = "What's another State name?").title()
-
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_states.append(answer_state)
         t = Turtle()
@@ -28,10 +32,3 @@ while len(guessed_states) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(answer_state)
     # t.write(state_data.state.item())
-
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop()   # To keep the screen open even if the code has finished running
-screen.exitonclick()
